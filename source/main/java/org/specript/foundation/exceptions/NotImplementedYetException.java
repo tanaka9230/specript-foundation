0b896d8a7b946970c7e42a4b45596f44847f1eaa
tanaka9230 <tanaka9230@users.noreply.github.com>
2017-05-20 02:30:11 +0900

    added NotImplementedYetException

---
+++ source/main/java/org/specript/foundation/exceptions/NotImplementedYetException.java
@@ -0,0 +1,12 @@
+/* ======================================---------------------------
+
+  --+ Specript Foundation Library v3.0 +--
+
+  Copyright (c) 2007-2017, Koichi Tanaka (@Tanaka9230)
+
+    ---------------------------====================================== */
+
+package org.specript.foundation.exceptions;
+
+public class NotImplementedYetException extends UnsupportedOperationException {
+}
